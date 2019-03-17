c69dbf24db33cb26f416b148ade345692fafa2a1
Anas Ahouzi <36398406+mouanisse@users.noreply.github.com>
2019-03-17 19:38:16 +0100

    Update Projet_reco.py

--- Projet_reco.py
+++ Projet_reco.py
@@ -44,7 +44,7 @@ model.add(keras.layers.Dropout(0.01))
 model.add(keras.layers.Conv2D(20, (3, 3), activation='relu')) #150
 model.add(keras.layers.BatchNormalization())
 model.add(keras.layers.MaxPooling2D(2, 2))
-model.add(keras.layers.Dropout(0.02))
+#model.add(keras.layers.Dropout(0.02))
 
 
 
