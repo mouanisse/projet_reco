25130d313952fdc0fbc346d8ce0507d652c1d9b8
Anas Ahouzi <36398406+mouanisse@users.noreply.github.com>
2019-03-17 21:35:50 +0100

    Update Projet_reco.py

--- Projet_reco.py
+++ Projet_reco.py
@@ -37,7 +37,7 @@ model.add(keras.layers.MaxPooling2D(2, 1))
 model.add(keras.layers.Conv2D(40, (3, 3), activation='relu')) #25
 model.add(keras.layers.BatchNormalization())
 model.add(keras.layers.MaxPooling2D(2, 2))    # divise par 2 les dimensions de l'image
-model.add(keras.layers.Dropout(0.08))
+model.add(keras.layers.Dropout(0.01))
 
 
 
