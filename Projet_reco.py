36d8f658bc87e3bc6729260f88d40153d42440b2
Anas Ahouzi <36398406+mouanisse@users.noreply.github.com>
2019-03-20 13:11:23 +0100

    Update Projet_reco.py

--- Projet_reco.py
+++ Projet_reco.py
@@ -23,7 +23,7 @@ input_shape = (96, 128, 3)
 # ('None' represents the batch_size).
 
 model = keras.Sequential()
-model.add(keras.layers.Permute((2,1,3)))
+#model.add(keras.layers.Permute((2,1,3)))
 
 # On utilise la fonction Permute pr changer le format de l'entrée
 # On aura alors la shape (128,96,3) , et ca donne des résultats bien meilleurs.
@@ -41,7 +41,7 @@ model.add(keras.layers.MaxPooling2D(2, 2))    # divise par 2 les dimensions de l
 
 
 
-model.add(keras.layers.Conv2D(80, (3, 3), activation='relu')) #150
+model.add(keras.layers.Conv2D(80, (3, 3), activation='relu')) 
 model.add(keras.layers.BatchNormalization())
 model.add(keras.layers.MaxPooling2D(2, 2))
 
