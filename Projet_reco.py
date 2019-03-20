fb16df35ed03e1b1131952ca3819d510e1368120
Anas Ahouzi <36398406+mouanisse@users.noreply.github.com>
2019-03-20 17:46:35 +0100

    Update Projet_reco.py

--- Projet_reco.py
+++ Projet_reco.py
@@ -53,6 +53,8 @@ model.add(keras.layers.MaxPooling2D(2, 2))
 
 model.add(keras.layers.Flatten())
 model.add(keras.layers.Dense(64, activation='relu'))
+model.add(keras.layers.Dropout(0.02))
+
 model.add(keras.layers.Dense(32, activation='relu'))
 model.add(keras.layers.Dropout(0.02))
 model.add(keras.layers.Dense(2, activation='softmax'))
