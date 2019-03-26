cab72d72a4a807322d0acbd18e75848d0d2bd04d
Anas Ahouzi <36398406+mouanisse@users.noreply.github.com>
2019-03-26 16:13:03 +0100

    Update Projet_reco.py

--- Projet_reco.py
+++ Projet_reco.py
@@ -28,34 +28,34 @@ model.add(keras.layers.Permute((2,1,3)))
 # On aura alors la shape (128,96,3) , et ca donne des résultats bien meilleurs.
 
 
-model.add(keras.layers.Conv2D(200, (5, 1), activation='relu')) #5
+model.add(keras.layers.Conv2D(20, (5, 1), activation='relu')) #5
 model.add(keras.layers.BatchNormalization())
 model.add(keras.layers.MaxPooling2D(2, 1))
 
 
-model.add(keras.layers.Conv2D(400, (3, 3), activation='relu'))
+model.add(keras.layers.Conv2D(40, (3, 3), activation='relu'))
 model.add(keras.layers.BatchNormalization())
 model.add(keras.layers.MaxPooling2D(2, 2))    # divise par 2 les dimensions de l'image
 
 
 
-model.add(keras.layers.Conv2D(800, (3, 3), activation='relu')) 
+model.add(keras.layers.Conv2D(80, (3, 3), activation='relu')) 
 model.add(keras.layers.BatchNormalization())
 model.add(keras.layers.MaxPooling2D(2, 2))
 
 
 
-model.add(keras.layers.Conv2D(1600, (3, 3), activation='relu')) 
+model.add(keras.layers.Conv2D(160, (3, 3), activation='relu')) 
 model.add(keras.layers.BatchNormalization())
 model.add(keras.layers.MaxPooling2D(2, 2))
 
 
 
 model.add(keras.layers.Flatten())
-model.add(keras.layers.Dense(320, activation='relu'))
+model.add(keras.layers.Dense(32, activation='relu'))
 model.add(keras.layers.BatchNormalization())
 
-model.add(keras.layers.Dense(640, activation='relu'))
+model.add(keras.layers.Dense(64, activation='relu'))
 model.add(keras.layers.BatchNormalization())
 model.add(keras.layers.Dense(35, activation='softmax'))
 
