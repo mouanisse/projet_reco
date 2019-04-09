21e58e32c463099e8e6b0d1efab0d9af9aefc612
Anas Ahouzi <36398406+mouanisse@users.noreply.github.com>
2019-04-09 23:33:25 +0200

    Update Projet_reco.py

--- Projet_reco.py
+++ Projet_reco.py
@@ -99,12 +99,12 @@ class Oyez_Oyez:
 
         model = keras.Sequential()
 
-        model.add(keras.layers.Conv2D(8, (5, 5), activation='relu'))
+        model.add(keras.layers.Conv2D(20, (5, 5), activation='relu'))
         model.add(keras.layers.BatchNormalization())
         model.add(keras.layers.MaxPooling2D(2, 2))
 
 
-        model.add(keras.layers.Conv2D(16, (5, 5), activation='relu'))
+        model.add(keras.layers.Conv2D(40, (5, 5), activation='relu'))
         model.add(keras.layers.BatchNormalization())
         model.add(keras.layers.MaxPooling2D(2, 2))
 
