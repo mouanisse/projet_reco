80f382e6f4353ab329921f275f3c1b9d8077f2b0
Anas Ahouzi <36398406+mouanisse@users.noreply.github.com>
2019-04-10 10:19:52 +0200

    Update Projet_reco.py

--- Projet_reco.py
+++ Projet_reco.py
@@ -57,17 +57,17 @@ class Oyez_Oyez:
         # On utilise la fonction Permute pr changer le format de l'entrée
         # On aura alors la shape (128,96,3) , et ca donne des résultats bien meilleurs.
 
-        model.add(keras.layers.Conv2D(20, (5, 5), activation='relu'))
+        model.add(keras.layers.Conv2D(20, (5, 1), activation='relu'))
         model.add(keras.layers.BatchNormalization())
         model.add(keras.layers.MaxPooling2D(2, 1))        # divise par 2 les dimensions de l'image
 
 
-        model.add(keras.layers.Conv2D(40, (5, 5), activation='relu'))
+        model.add(keras.layers.Conv2D(40, (3, 3), activation='relu'))
         model.add(keras.layers.BatchNormalization())
         model.add(keras.layers.MaxPooling2D(2, 2))  # divise par 4 les dimensions de l'image
 
 
-        model.add(keras.layers.Conv2D(80, (5, 5), activation='relu'))
+        model.add(keras.layers.Conv2D(80, (4, 4), activation='relu'))
         model.add(keras.layers.BatchNormalization())
         model.add(keras.layers.MaxPooling2D(2, 2))
         
