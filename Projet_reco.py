4bdd2126eae2af06f4b2e2f031d396f5ba1b1bb6
Anas Ahouzi <36398406+mouanisse@users.noreply.github.com>
2019-04-11 13:29:48 +0200

    Update Projet_reco.py

--- Projet_reco.py
+++ Projet_reco.py
@@ -123,10 +123,12 @@ class Oyez_Oyez:
         model = keras.Sequential()
 
         model.add(keras.layers.Conv2D(8, (5, 5), strides=(1, 1), input_shape=(129, 129, 1), padding='same', activation='relu'))
+        model.add(keras.layers.BatchNormalization())
         model.add(keras.layers.MaxPooling2D(pool_size=(2,2), strides=(2,2)))
 
 
         model.add(keras.layers.Conv2D(16, (5, 5), padding='same', activation='relu'))
+        model.add(keras.layers.BatchNormalization())
         model.add(keras.layers.MaxPooling2D(pool_size=(2,2), strides=(2,2)))
         
 
