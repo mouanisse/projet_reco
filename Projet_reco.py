d073c522d65d1265690109aaec94ea0810f41bd7
Anas Ahouzi <36398406+mouanisse@users.noreply.github.com>
2019-04-12 14:50:44 +0200

    Update Projet_reco.py

--- Projet_reco.py
+++ Projet_reco.py
@@ -160,7 +160,7 @@ class Oyez_Oyez:
         model = self.create_word_model()
         model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy']
 
-        model.fit(self.training_word_data, self.training_word_label, batch_size=100, epochs=7, validation_data=(self.validation_word_data, self.validation_word_label) )
+        #model.fit(self.training_word_data, self.training_word_label, batch_size=100, epochs=7, validation_data=(self.validation_word_data, self.validation_word_label) )
 
         score = model.evaluate(testing_word_data, testing_word_label, verbose=0)
         print('Test loss:', score[0])
