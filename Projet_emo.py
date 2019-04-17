105fda915430090d345c468f4951eef1716574e8
Anas Ahouzi <36398406+mouanisse@users.noreply.github.com>
2019-04-18 01:31:16 +0200

    Update Projet_emo.py

--- Projet_emo.py
+++ Projet_emo.py
@@ -19,7 +19,7 @@ test_data, val_data, test_labels, val_labels = train_test_split(test_val_data, t
 
 model = keras.Sequential()
 
-model.add(keras.layers.Conv1D(1024, 5, padding='same', input_shape=(259, 1), activation='relu'))
+model.add(keras.layers.Conv1D(512, 5, padding='same', input_shape=(259, 1), activation='relu'))
 model.add(keras.layers.BatchNormalization())
 
 model.add(keras.layers.Conv1D(512, 5, padding='same', activation='relu'))
@@ -30,7 +30,10 @@ model.add(keras.layers.MaxPooling1D(pool_size=8))
 model.add(keras.layers.Conv1D(256, 5, padding='same', activation='relu'))
 model.add(keras.layers.BatchNormalization())
 
-model.add(keras.layers.Conv1D(128, 5, padding='same', activation='relu'))
+model.add(keras.layers.Conv1D(256, 5, padding='same', activation='relu'))
+model.add(keras.layers.BatchNormalization())
+
+model.add(keras.layers.Conv1D(256, 5, padding='same', activation='relu'))
 model.add(keras.layers.BatchNormalization())
 
 model.add(keras.layers.MaxPooling1D(pool_size=8))
