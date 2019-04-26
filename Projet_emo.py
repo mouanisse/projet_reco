51a9d60f55bc3f0b2332c39f85d20ce625b7be1b
Anas Ahouzi <36398406+mouanisse@users.noreply.github.com>
2019-04-26 19:05:16 +0200

    Update Projet_emo.py

--- Projet_emo.py
+++ Projet_emo.py
@@ -19,10 +19,10 @@ test_data, val_data, test_labels, val_labels = train_test_split(test_val_data, t
 
 model = keras.Sequential()
 
-model.add(keras.layers.Conv1D(64, 5, padding='same', input_shape=(517,1), activation='relu'))
+model.add(keras.layers.Conv1D(128, 5, padding='same', input_shape=(517,1), activation='relu'))
 model.add(keras.layers.BatchNormalization())
 
-model.add(keras.layers.Conv1D(128, 5, padding='same', activation='relu'))
+model.add(keras.layers.Conv1D(256, 5, padding='same', activation='relu'))
 model.add(keras.layers.BatchNormalization())
 
 model.add(keras.layers.MaxPooling1D(pool_size=8))
