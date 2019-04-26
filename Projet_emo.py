63fdabeffa70a5bc7cae28c952cdb0656936431c
Anas Ahouzi <36398406+mouanisse@users.noreply.github.com>
2019-04-26 15:43:35 +0100

    Update Projet_emo.py

--- Projet_emo.py
+++ Projet_emo.py
@@ -21,22 +21,24 @@ model = keras.Sequential()
 
 model.add(keras.layers.Conv1D(32, 5, padding='same', input_shape=(517,1), activation='relu'))
 model.add(keras.layers.BatchNormalization())
+model.add(keras.layers.MaxPooling1D(pool_size=8))
 
 model.add(keras.layers.Conv1D(64, 5, padding='same', activation='relu'))
 model.add(keras.layers.BatchNormalization())
-
 model.add(keras.layers.MaxPooling1D(pool_size=8))
 
 model.add(keras.layers.Conv1D(128, 5, padding='same', activation='relu'))
 model.add(keras.layers.BatchNormalization())
+model.add(keras.layers.MaxPooling1D(pool_size=8))
 
 model.add(keras.layers.Conv1D(256, 5, padding='same', activation='relu'))
 model.add(keras.layers.BatchNormalization())
+model.add(keras.layers.MaxPooling1D(pool_size=8))
 
 model.add(keras.layers.Conv1D(512, 5, padding='same', activation='relu'))
 model.add(keras.layers.BatchNormalization())
-
 model.add(keras.layers.MaxPooling1D(pool_size=8))
+
 
 model.add(keras.layers.Flatten())
 model.add(keras.layers.Dense(7, activation='softmax'))
