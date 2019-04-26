afbb78ba8cbb761941b57f0ed27169b994ad39bd
Anas Ahouzi <36398406+mouanisse@users.noreply.github.com>
2019-04-26 14:52:23 +0200

    Update Projet_emo.py

--- Projet_emo.py
+++ Projet_emo.py
@@ -19,27 +19,18 @@ test_data, val_data, test_labels, val_labels = train_test_split(test_val_data, t
 
 model = keras.Sequential()
 
-model.add(keras.layers.Conv1D(512, 5, padding='same', input_shape=(517,1), activation='relu'))
+model.add(keras.layers.Conv1D(20, 5, padding='same', input_shape=(517,1), activation='relu'))
 model.add(keras.layers.BatchNormalization())
 
-model.add(keras.layers.Conv1D(512, 5, padding='same', activation='relu'))
+model.add(keras.layers.Conv1D(40, 5, padding='same', activation='relu'))
 model.add(keras.layers.BatchNormalization())
 
 model.add(keras.layers.MaxPooling1D(pool_size=8))
 
-model.add(keras.layers.Conv1D(256, 5, padding='same', activation='relu'))
+model.add(keras.layers.Conv1D(80, 5, padding='same', activation='relu'))
 model.add(keras.layers.BatchNormalization())
 
-model.add(keras.layers.Conv1D(256, 5, padding='same', activation='relu'))
-model.add(keras.layers.BatchNormalization())
-
-model.add(keras.layers.Conv1D(256, 5, padding='same', activation='relu'))
-model.add(keras.layers.BatchNormalization())
-
-model.add(keras.layers.Conv1D(256, 5, padding='same', activation='relu'))
-model.add(keras.layers.BatchNormalization())
-
-model.add(keras.layers.Conv1D(256, 5, padding='same', activation='relu'))
+model.add(keras.layers.Conv1D(160, 5, padding='same', activation='relu'))
 model.add(keras.layers.BatchNormalization())
 
 model.add(keras.layers.MaxPooling1D(pool_size=8))
