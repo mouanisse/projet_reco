f251dc9f8511187dec25f6c3965e3edba7f3fe9f
Anas Ahouzi <36398406+mouanisse@users.noreply.github.com>
2019-04-26 20:12:00 +0200

    Update Projet_emo.py

--- Projet_emo.py
+++ Projet_emo.py
@@ -7,10 +7,10 @@ from sklearn.model_selection import train_test_split
 
 #********************************************* Loading the dataset **************************************************
 
-train_data = np.load('/content/drive/My Drive/Colab Notebooks/emotion_dataset/train_emotions.npy')
-test_val_data = np.load('/content/drive/My Drive/Colab Notebooks/emotion_dataset/test_emotions.npy')
-train_labels = np.load('/content/drive/My Drive/Colab Notebooks/emotion_dataset/train_labels.npy')
-test_val_labels = np.load('/content/drive/My Drive/Colab Notebooks/emotion_dataset/test_labels.npy')
+train_data = np.load('/content/drive/My Drive/Colab Notebooks/emotion_dataset/train_emotions3.npy')
+test_val_data = np.load('/content/drive/My Drive/Colab Notebooks/emotion_dataset/test_emotions3.npy')
+train_labels = np.load('/content/drive/My Drive/Colab Notebooks/emotion_dataset/train_labels3.npy')
+test_val_labels = np.load('/content/drive/My Drive/Colab Notebooks/emotion_dataset/test_labels3.npy')
 test_data, val_data, test_labels, val_labels = train_test_split(test_val_data, test_val_labels, test_size=0.5, random_state=42)
 
 
@@ -46,7 +46,7 @@ model.add(keras.layers.MaxPooling1D(pool_size=8))
 
 
 model.add(keras.layers.Flatten())
-model.add(keras.layers.Dense(8, activation='softmax'))
+model.add(keras.layers.Dense(7, activation='softmax'))
 
 
 model.summary()
@@ -56,7 +56,7 @@ model.summary()
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-model.fit(train_data, train_labels, batch_size=100, epochs=50, validation_data=(val_data, val_labels))
+model.fit(train_data, train_labels, batch_size=500, epochs=50, validation_data=(val_data, val_labels))
 
 score = model.evaluate(test_data, test_labels, verbose=0)
 model.save("emotion_model_mfcc.h5")
