c77cd7392fa4c8608c0fb383f94a1b58851a832a
Anas Ahouzi <36398406+mouanisse@users.noreply.github.com>
2019-04-26 18:32:50 +0200

    Update Projet_emo.py

--- Projet_emo.py
+++ Projet_emo.py
@@ -25,10 +25,7 @@ model.add(keras.layers.BatchNormalization())
 model.add(keras.layers.Conv1D(64, 5, padding='same', activation='relu'))
 model.add(keras.layers.BatchNormalization())
 
-model.add(keras.layers.MaxPooling1D(pool_size=4))
-
-model.add(keras.layers.Conv1D(256, 5, padding='same', activation='relu'))
-model.add(keras.layers.BatchNormalization())
+model.add(keras.layers.MaxPooling1D(pool_size=8))
 
 model.add(keras.layers.Conv1D(256, 5, padding='same', activation='relu'))
 model.add(keras.layers.BatchNormalization())
@@ -48,7 +45,7 @@ model.add(keras.layers.BatchNormalization())
 model.add(keras.layers.Conv1D(512, 5, padding='same', activation='relu'))
 model.add(keras.layers.BatchNormalization())
 
-model.add(keras.layers.MaxPooling1D(pool_size=4))
+model.add(keras.layers.MaxPooling1D(pool_size=8))
 
 
 model.add(keras.layers.Flatten())
