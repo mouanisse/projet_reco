73f161f989d12d7147cd491f8b6637461de827cd
Anas Ahouzi <36398406+mouanisse@users.noreply.github.com>
2019-04-27 20:12:15 +0200

    Update Projet_emo.py

--- Projet_emo.py
+++ Projet_emo.py
@@ -36,6 +36,9 @@ model.add(keras.layers.BatchNormalization())
 model.add(keras.layers.Conv1D(256, 5, padding='same', activation='relu'))
 model.add(keras.layers.BatchNormalization())
 
+model.add(keras.layers.Conv1D(512, 5, padding='same', activation='relu'))
+model.add(keras.layers.BatchNormalization())
+
 model.add(keras.layers.MaxPooling1D(pool_size=8))
 
 
