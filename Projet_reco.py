399285ef561a36b8fe1d0961edf629d2bec72589
Anas Ahouzi <36398406+mouanisse@users.noreply.github.com>
2019-05-01 19:35:34 +0200

    Update Projet_reco.py

--- Projet_reco.py
+++ Projet_reco.py
@@ -3,30 +3,6 @@ from tensorflow import keras
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-################################################ Oyez_Oyez DATASET ##############################################
-
-training_word_data = np.load('/content/drive/My Drive/Colab Notebooks/5_words_dataset/reduce_train_set.npy')
-testing_word_data = np.load('/content/drive/My Drive/Colab Notebooks/5_words_dataset/reduce_test_set.npy')
-validation_word_data = np.load('/content/drive/My Drive/Colab Notebooks/5_words_dataset/reduce_valid_set.npy')
-
-training_word_label = np.load('/content/drive/My Drive/Colab Notebooks/5_words_dataset/reduce_train_label.npy')
-testing_word_label = np.load('/content/drive/My Drive/Colab Notebooks/5_words_dataset/reduce_test_label.npy')
-validation_word_label = np.load('/content/drive/My Drive/Colab Notebooks/5_words_dataset/reduce_validation_label.npy')
-
-
-################################################ RAVDESS DATASET ##############################################
-
-training_emotion_data = np.load('/content/drive/My Drive/Colab Notebooks/emotion_dataset/training_emotion_images.npy')
-testing_emotion_data = np.load('/content/drive/My Drive/Colab Notebooks/emotion_dataset/testing_emotion_images.npy')
-validation_emotion_data = np.load('/content/drive/My Drive/Colab Notebooks/emotion_dataset/validation_emotion_images.npy')
-
-training_emotion_label = np.load('/content/drive/My Drive/Colab Notebooks/emotion_dataset/training_emotion_labels.npy')
-testing_emotion_label = np.load('/content/drive/My Drive/Colab Notebooks/emotion_dataset/testing_emotion_labels.npy')
-validation_emotion_label = np.load('/content/drive/My Drive/Colab Notebooks/emotion_dataset/validation_emotion_labels.npy')
-
-training_emotion_data = training_emotion_data.reshape((-1, 129, 129, 1))
-testing_emotion_data = testing_emotion_data.reshape((-1, 129, 129, 1))
-validation_emotion_data = validation_emotion_data.reshape((-1, 129, 129, 1))
 
 
 ########################################### 3A DATASET ################################################
@@ -67,23 +43,8 @@ test_images_res = test_data.reshape((-1, 129, 129, 1))
 
 class Oyez_Oyez:
 
-    def __init__(self, training_word_data, testing_word_data, validation_word_data, training_word_label,
-                 testing_word_label, validation_word_label, training_emotion_data, testing_emotion_data,
-                 validation_emotion_data, training_emotion_label, testing_emotion_label, validation_emotion_label):
+    def __init__(self):
 
-        self.training_word_data = training_word_data
-        self.testing_word_data = testing_word_data
-        self.validation_word_data = validation_word_data
-        self.training_word_label = training_word_label
-        self.testing_word_label = testing_word_label
-        self.validation_word_label = validation_word_label
-
-        self.training_emotion_data = training_emotion_data
-        self.testing_emotion_data = testing_emotion_data
-        self.validation_emotion_data = validation_emotion_data
-        self.training_emotion_label = training_emotion_label
-        self.testing_emotion_label = testing_emotion_label
-        self.validation_emotion_label = validation_emotion_label
 
 
     # 2D Convolutional Model
@@ -203,9 +164,6 @@ class Oyez_Oyez:
 
 
 
-obj = Oyez_Oyez(training_word_data, testing_word_data, validation_word_data, training_word_label,
-                 testing_word_label, validation_word_label, training_emotion_data, testing_emotion_data,
-                 validation_emotion_data, training_emotion_label, testing_emotion_label, validation_emotion_label )
-
+obj = Oyez_Oyez()
 
 obj.train_emotion_model()
