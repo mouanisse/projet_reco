9c635b029e9ebe9fbd00c35def12d98b98e46d15
Anas Ahouzi <36398406+mouanisse@users.noreply.github.com>
2019-05-07 14:15:07 +0200

    Update Projet_emo.py

--- Projet_emo.py
+++ Projet_emo.py
@@ -7,10 +7,10 @@ from sklearn.model_selection import train_test_split
 
 #********************************************* Loading the dataset **************************************************
 
-train_data = np.load('/content/drive/My Drive/Colab Notebooks/emotion_dataset/train_emotions_3_enseirb.npy')
-test_val_data = np.load('/content/drive/My Drive/Colab Notebooks/emotion_dataset/test_emotions_3_enseirb.npy')
-train_labels = np.load('/content/drive/My Drive/Colab Notebooks/emotion_dataset/train_labels_3_enseirb.npy')
-test_val_labels = np.load('/content/drive/My Drive/Colab Notebooks/emotion_dataset/test_labels_3_enseirb.npy')
+train_data = np.load('/content/drive/My Drive/Colab Notebooks/emotion_dataset/train_emotions_3_RAVDESS.npy')
+test_val_data = np.load('/content/drive/My Drive/Colab Notebooks/emotion_dataset/test_emotions_3_RAVDESS.npy')
+train_labels = np.load('/content/drive/My Drive/Colab Notebooks/emotion_dataset/train_labels_3_RAVDESS.npy')
+test_val_labels = np.load('/content/drive/My Drive/Colab Notebooks/emotion_dataset/test_labels_3_RAVDESS.npy')
 test_data, val_data, test_labels, val_labels = train_test_split(test_val_data, test_val_labels, test_size=0.5, random_state=42)
 
 
@@ -19,13 +19,19 @@ test_data, val_data, test_labels, val_labels = train_test_split(test_val_data, t
 
 model = keras.Sequential()
 
-model.add(keras.layers.Conv1D(64, 5, padding='same', input_shape=(345,1), activation='relu'))
+model.add(keras.layers.Conv1D(64, 5, padding='same', input_shape=(259,1), activation='relu'))
 model.add(keras.layers.BatchNormalization())
 
 model.add(keras.layers.Conv1D(128, 5, padding='same', activation='relu'))
 model.add(keras.layers.BatchNormalization())
 
 model.add(keras.layers.MaxPooling1D(pool_size=8))
+
+model.add(keras.layers.Conv1D(256, 5, padding='same', activation='relu'))
+model.add(keras.layers.BatchNormalization())
+
+model.add(keras.layers.Conv1D(256, 5, padding='same', activation='relu'))
+model.add(keras.layers.BatchNormalization())
 
 model.add(keras.layers.Conv1D(256, 5, padding='same', activation='relu'))
 model.add(keras.layers.BatchNormalization())
@@ -44,7 +50,7 @@ model.summary()
 
 #**************************************** Compile and train our CNN model *******************************************
 
-checkpoint = keras.callbacks.ModelCheckpoint('model4  -{epoch:03d}-{acc:03f}-{val_acc:03f}.h5', verbose=1, monitor='val_acc',save_best_only=True, mode='auto')
+checkpoint = keras.callbacks.ModelCheckpoint('model-{epoch:03d}-{acc:03f}-{val_acc:03f}.h5', verbose=1, monitor='val_acc',save_best_only=True, mode='auto')
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
